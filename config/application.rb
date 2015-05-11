295342ca63e6f75607e3e7de325b02a048cf2514
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-05-11 23:29:44 +0200

    added possibility to create development and test databases stubs from rake task with rake create_external

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,36 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'yaml'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+db_conf = YAML::load(File.open(File.join("./",'config','database.yml')))
+
+DB_CONFIG = {}
+DB_CONFIG["stsc_feedzilla_downloader"] = db_conf["stsc_feedzilla_downloader"][Rails.env.to_str]
+DB_CONFIG["stsc_yahoo_downloader"] = db_conf["stsc_yahoo_downloader"][Rails.env.to_str]
+
+module ExternalBase
+  class ExternalStscDatabaseTasks
+      include ActiveRecord::Tasks::DatabaseTasks
+  end
+  class Creator
+    def self.create_database(config)
+        if (Rails.env.production?)
+          puts "Rake Task defined only not for production environment"
+        else
+            a = ExternalStscDatabaseTasks.new
+            a.register_task(/mysql/,        ActiveRecord::Tasks::MySQLDatabaseTasks)
+            a.register_task(/postgresql/,   ActiveRecord::Tasks::PostgreSQLDatabaseTasks)
+            a.register_task(/sqlite/,       ActiveRecord::Tasks::SQLiteDatabaseTasks)
+            a.create(config)
+        end
+    end
+  end
+end
 
 module StscFrontendKare
   class Application < Rails::Application
