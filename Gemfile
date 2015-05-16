171ae7c300fe2383f7d996b79b72557860f90d8b
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-05-16 21:55:53 +0200

    added feedzilla downloader statistics csv as test fixtures

--- Gemfile
+++ Gemfile
@@ -43,3 +43,10 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Rails Csv Fixtures
+# gem 'rails-csv-fixtures', '0.0.1'
+
+
+
+
--- test/models/feedzilla_downloader_statistic_test.rb
+++ test/models/feedzilla_downloader_statistic_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class FeedzillaDownloaderStatisticTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    v = FeedzillaDownloaderStatistic.all
+    assert_equal 0, v.count
+  end
 end
